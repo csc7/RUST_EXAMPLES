ee7076a6bbc0cd69d05bfaadf9a48dc698c61195
Carlos Colombo <csc7@hw.ac.uk>
2021-12-29 17:54:40 +0000

    Final version of fizz_buzz

--- fizz_buzz.rs
+++ fizz_buzz.rs
@@ -1,15 +1,72 @@
 // Print integers from 1 to N, printing “Fizz” if the integer is divisible
 // by 3, “Buzz” if divisible by 5, and “FizzBuzz” if divisible by both 3 and 5
 
+
+// LIBRARIES
 use std::io;
+
+
+// THIS FUNCTION CONTROLS THE EXECUTION/REPETITION OF THE PROGRAM
+fn stop_or_continue_program() -> bool {
+    println!("Keep running?");
+    println!("Enter 'exit' to leave or anything else to continue");
+    let mut answ = String::new();
+    io::stdin().read_line(&mut answ).expect("Not a valid string");
+    // The solution using ".trim()" (versus not using it) was found here:
+    // http://danielnill.com/rust_tip_compairing_strings
+    // © 2018 Daniel Nill.
+    // Accessed on December 29th, 2021, at 18:23.
+    if answ.trim() == "exit" {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+
+// PRINCIPAL FUNCTION, IT COMPUTES THE MULTIPLES AND
+// ASSIGN THE CORRESPONDING WORD
+fn fuzz_bizz_multiples(num: isize) {
+
+    // All possible answers and answer to print
+    let all_answers = ["Fizz", "Buzz", "FizzBuzz", ""];
+    let mut answer = all_answers[3];
+
+    // Check all values
+    for i in 1..num+1 {
+        // If divisible by both 3 and 5, assign, print and go to
+        // next iteration, resetTing answer to "none", as it is
+        // not necessary to check 3 and 5 individually
+        if i % 3 == 0 && i % 5 == 0 {
+                answer = all_answers[2];
+                println!("{}: {}", i, answer);
+                answer = all_answers[3];
+                continue;
+            }
+        // Check if divisible by 3 and assign answer
+        if i % 3 == 0 {
+                answer = all_answers[0];
+            }
+        // If not by 3, check if divisible by 5 and assign answer
+        if i % 5 == 0 {
+                    answer = all_answers[1];
+                }
+        // Print answer and reset it to "none"       
+        println!("{}: {}", i, answer);
+        answer = all_answers[3];        
+    }
+}
+
 
 fn main() {
 
+    // Initialize variable that controls the execution of program
     let mut keep_running = true;
 
     while keep_running {
       
-        // Welcome message showing maximum integer value for i32 type
+        // Welcome message
         println!("\n");
         println!("===================");
         println!("FIZZ BUZZ MULTIPLES");
@@ -42,7 +99,7 @@ fn main() {
                 continue;
             },
         };
-        // End Data Input in Rust
+        // End credit for Data Input in Rust
 
         // Using "isize" type to account for negative integers (and so give
         // the user a message to enter values from 1 to N, as requested) and
@@ -74,53 +131,4 @@ fn main() {
     println!("Program Closed");
     println!("==============");
     println!("\n");
-}
-
-fn stop_or_continue_program() -> bool {
-    println!("Keep running?");
-    println!("Enter 'exit' to leave or anything else to continue");
-    let mut answ = String::new();
-    io::stdin().read_line(&mut answ).expect("Not a valid string");
-    // The solution using ".trim()" (versus not using it) was found here:
-    // http://danielnill.com/rust_tip_compairing_strings
-    // © 2018 Daniel Nill.
-    // Accessed on December 29th, 2021, at 18:23.
-    if answ.trim() == "exit" {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-
-
-
-fn fuzz_bizz_multiples(num: isize) {
-    // All possible answers and answer to print
-    let all_answers = ["Fizz", "Buzz", "FizzBuzz", ""];
-    let mut answer = all_answers[3];
-
-    // Check all values
-    for i in 1..num+1 {
-        // If divisible by both 3 and 5, assign, print and go to
-        // next iteration, reseting answer to "none", as it is
-        // not necessary to check 3 and 5 individually
-        if i % 3 == 0 && i % 5 == 0 {
-                answer = all_answers[2];
-                println!("{}: {}", i, answer);
-                answer = all_answers[3];
-                continue;
-            }
-        // Check if divisible by 3 and assign answer
-        if i % 3 == 0 {
-                answer = all_answers[0];
-            }
-        // If not by 3, check if divisible by 5 and assign answer
-        if i % 5 == 0 {
-                    answer = all_answers[1];
-                }
-        // Print answer and reset it to "none"       
-        println!("{}: {}", i, answer);
-        answer = all_answers[3];        
-    }
 }
